a1dde03750085446f03b13397665222dc41f1156
Matt Schmitter <matt.schmitter@gmail.com>
2015-11-20 00:38:39 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catsquiz::Application.config.secret_key_base = 'f726b9387df24ccbef93add5ff0befbcca43793dab9b8e1d8d1d76f400ed4074093e389c75b5780e9abb488ee206a82c9b3137f4706ed89dbd9165f8a77d847d'
